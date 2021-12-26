661c9902998456eb7ca95d7d38aa039cf58979ab
quoc223 <92385492+quoc223@users.noreply.github.com>
2021-12-26 12:12:33 +0700

    Add files via upload

---
+++ B1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class B1 {
+	public static void main(String[] args) {
+		int n;
+		Scanner sc=new Scanner(System.in);
+		do {
+		System.out.print("Nhap vao 10 so nguyen");
+		n=sc.nextInt();
+		}while (n<0);
+	int	array[]=new int[n];
+	System.out.println("Nhap phan tu vao mang");
+	for (int i=0;i<n;i++) 
+	{
+System.out.print("Nhap phan tu thu "+i+":");
+array[i] =sc.nextInt();
+	}
+	System.out.print("Day so nguyen:");
+	for (int i = 0; i < n; i++) {
+        System.out.print(array[i] + "\t");
+	}
+	}
+
+}
